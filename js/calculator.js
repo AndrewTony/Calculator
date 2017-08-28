d8a2c32a79225234cf18c88e4282870ccc5f9deb
tonyishak <tony.ishak234@gmail.com>
2017-08-28 15:19:22 -0400

    Calculator working

--- js/calculator.js
+++ js/calculator.js
@@ -3,15 +3,15 @@ var leftDisplay = document.getElementById("calcDisplay1");
 var signBtns = document.getElementsByClassName("calcSignButton");
 var centerDisplay = document.getElementById("calcDisplay2");
 var rightDisplay = document.getElementById("calcDisplay3");
+var equalsSign = document.getElementById("equals")
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
 
-for(var i = 0; i < btns.length; i++){
-    btns[i].addEventListener("click", function(){
-
-        if(centerDisplay.getAttribute("value") !== "" && leftDisplay.getAttribute("value") !== ""){
+        if (centerDisplay.getAttribute("value") !== "" && leftDisplay.getAttribute("value") !== "") {
             var value = rightDisplay.getAttribute("value");
             value += this.getAttribute("value");
             rightDisplay.setAttribute("value", value);
-        }else{
+        } else {
             var value = leftDisplay.getAttribute("value");
             value = value + this.getAttribute("value");
 
@@ -20,10 +20,33 @@ for(var i = 0; i < btns.length; i++){
     })
 }
 
-for(var i = 0; i < signBtns.length; i++){
-    signBtns[i].addEventListener("click", function(){
+for (var i = 0; i < signBtns.length; i++) {
+    signBtns[i].addEventListener("click", function () {
         centerDisplay.setAttribute("value", this.getAttribute("value"));
     })
 }
+equalsSign.addEventListener("click", function () {
+    var centerValue = centerDisplay.getAttribute("value");
+    var leftValue = parseInt(leftDisplay.getAttribute("value"));
+    var rightValue = parseInt(rightDisplay.getAttribute("value"));
+    var total;
 
+    switch (centerValue) {
+        case "+":
+            total = leftValue + rightValue;
+            break;
+        case "-":
+            total = leftValue - rightValue;
+            break;
+        case "/":
+            total = leftValue / rightValue;
+            break;
+        case "x":
+            total = leftValue * rightValue;
+            break;
+    }
+    leftDisplay.setAttribute("value", total);
+    centerDisplay.setAttribute("value", "");
+    rightDisplay.setAttribute("value", "");
 
+})
